77c9a70f661539355741a5993c9c94c9b1000222
Mirsaid25 <siddikovmirsaid01@gmail.com>
2024-02-16 18:31:47 +0500

    add action for billboards table

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -85,12 +85,13 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
             setLodaing(true);
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
-            router.push("/");
+            router.push(`/${params.storeId}/billboards`);
             toast.success("Bilboard deleted successfully.");
         } catch (error) {
             toast.error(
                 "Make sure you removed all categories using this billboard first."
             );
+            console.log(error);
         } finally {
             setLodaing(false);
             setOpen(false);
